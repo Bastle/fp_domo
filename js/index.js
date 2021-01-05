d8c2db11fdb7d96024b8e2cffb6523a1eace03d3
xmly <xmly@xmlydeMacBook-Pro.local>
2021-01-05 16:45:52 +0800

    prettier

--- js/index.js
+++ js/index.js
@@ -1,38 +1,31 @@
 requirejs.config({
   paths: {
-    ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.13.0/ramda.min',
-    jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min'
-  }
+    ramda: "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.13.0/ramda.min",
+    jquery: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min",
+  },
 });
 
-require([
-    'ramda',
-    'jquery'
-  ],
-  function (_, $) {
+require(["ramda", "jquery"], function (_, $) {
+  const Impure = {
+    getJSON: _.curry(function (callback, url) {
+      $.getJSON(url, callback);
+    }),
 
-    const Impure = {
-      getJSON: _.curry(function(callback, url) {
-        $.getJSON(url, callback);
-      }),
+    setHtml: _.curry(function (sel, html) {
+      $(sel).html(html);
+    }),
+  };
 
-      setHtml: _.curry(function(sel, html) {
-        $(sel).html(html);
-      })
-    };
- 
+  const img = (url) => $("<img />", { src: url });
+  const url = (t) =>
+    `https://api.flickr.com/services/feeds/photos_public.gne?tags=${t}&format=json&jsoncallback=?`;
 
-    const img = url => $('<img />', { src: url })
-    const url = t => `https://api.flickr.com/services/feeds/photos_public.gne?tags=${t}&format=json&jsoncallback=?`
-    
+  const mediaUrl = _.compose(_.prop("m"), _.prop("media"));
+  const srcs = _.compose(_.map(mediaUrl), _.prop("items"));
+  const images = _.compose(_.map(img), srcs);
+  const renderImages = _.compose(Impure.setHtml("body"), images);
+  // callback：renderImages
+  const app = _.compose(Impure.getJSON(renderImages), url);
 
-    const mediaUrl = _.compose(_.prop('m'), _.prop('media'));
-    const srcs = _.compose(_.map(mediaUrl), _.prop('items'));
-    const images = _.compose(_.map(img), srcs);
-    const renderImages = _.compose(Impure.setHtml("body"), images);
-    // callback：renderImages
-    const app = _.compose(Impure.getJSON(renderImages), url);
-  
-    app("cats");
-
-  });
+  app("cats");
+});
